04ffb792d9adb0fb09414562c88a70b39e8b64f2
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-18 15:10:36 -0400

    commit tree dd9b6b11e687f54a90bc8d7e624854894d8cef6f

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "luciana"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        # binding.pry
+        session[:item] = params[:item]
+        @session = session[:item]
+
+        erb :checkout
+    end
 end
